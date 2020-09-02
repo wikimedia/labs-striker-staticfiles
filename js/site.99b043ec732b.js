6eafd30720ca5719c6e48525da58e2fea604a565
Bryan Davis <bd808@wikimedia.org>
2020-09-02 11:51:12 -0600

    Remove stale assets
    The rebuild process that is used for this repo adds new files, but
    leaves behind files that are no longer listed in the staticfiles.json
    manifest. This is done so that cached pages can still fetch the version
    of assets that were needed at the time the page was generated. Depending
    on the release cycle, these historic files can be cleaned up after 1-2
    releases. This cleanup purges all tracked files that are not used in
    the upcoming 2020-09-02 release or the prior (active) release.
    
    Change-Id: I17bf277fab80131f9ce40b942432f386054ae068

--- js/site.99b043ec732b.js
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) 2016 Wikimedia Foundation and contributors.
- * Licensed under the GPL v3+ license
- */
-$(function () {
-    "use strict";
-    $('[data-toggle="tooltip"]').tooltip();
-})
